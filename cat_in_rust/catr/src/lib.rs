203908a85b86c08fafa4c48d3fb72cb8ff9e2c09
XIAOZHUXUEJAVA <2393751426@qq.com>
2023-05-07 19:43:03 +0800

    init cat

---
+++ cat_in_rust/catr/src/lib.rs
@@ -0,0 +1,74 @@
+use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>, // 可以cat多个文件
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("XIAOZHUXUEJAVA")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"), // 默认值是这个  如果有别的值 就不是了
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false)
+                .conflicts_with("number"),
+        )
+        .get_matches();
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(e) => eprintln!("{}: {}", filename, e),
+            Ok(file) => {
+                if config.number_lines {
+                    println!("number_lines");
+                } else if config.number_nonblank_lines {
+                    println!("number_nonblank_lines")
+                } else {
+                    println!("lines")
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))), // input
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))), // read file
+    }
+}
